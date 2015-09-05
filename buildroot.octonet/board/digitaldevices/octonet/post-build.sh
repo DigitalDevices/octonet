72d183c3e6760629d9617ea503c801a07a881547
mvoelkel <mvoelkel@digitaldevices.de>
2015-09-05 18:39:08 +0200

    Added creating of /config to post-build.sh
    git doesn't like empty subdirectories

--- buildroot.octonet/board/digitaldevices/octonet/post-build.sh
+++ buildroot.octonet/board/digitaldevices/octonet/post-build.sh
@@ -3,3 +3,8 @@ TARGETDIR=$1
 grep -q config $TARGETDIR/etc/fstab || echo -e "/dev/ubi0_0\t\t/config\tubifs\tdefaults\t0\t0" >> $TARGETDIR/etc/fstab
 grep -q pts $TARGETDIR/etc/securetty || echo -e "pts/0\npts/1\npts/2\npts/3" >> $TARGETDIR/etc/securetty
 
+mkdir -p $TARGETDIR/config
+
+grep -q admin $TARGETDIR/etc/passwd || echo -e "admin:x:200:200:nobody:/home:/bin/false" >> $TARGETDIR/etc/passwd
+grep -q admin $TARGETDIR/etc/shadow || echo -e "admin:WiBKbsJTSQ8dc:13514:0:99999:7:::" >> $TARGETDIR/etc/shadow
+
